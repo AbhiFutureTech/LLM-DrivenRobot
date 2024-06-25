592074f1e1c8f80286934dfaa9b6b261560affd5
Abhijit Patil <157373320+patilabhi20@users.noreply.github.com>
2024-06-25 19:43:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gensim',
+    name='Robotics',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     license=open('LICENSE').read(),
     zip_safe=False,
-    description="GenSim: Generating Robotic Simulation Tasks via Large Language Models.",
-    author='Lirui Wang',
-    author_email='liruiw@mit.edu',
-    url='https://liruiw.github.io/gensim',
+    description="Robotic Tasks via Large Language Models.",
+    author='Abhijit Patil',
+    author_email='abhianantapatil@gmail.com',
+    url='https://github.com/patilabhi20/Robotic-Tasks-via-Large-Language-Models',
    #  install_requires=[line for line in open('requirements.txt').readlines() if "@" not in line],
     keywords=['Large Language Models', 'Simulation', 'Vision Language Grounding', 'Robotics', 'Manipulation'],
 )
